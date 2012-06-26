cf6d8612694a4e620e2524fe66c515a9fbb371ea
Mark Cooper <mark.cooper@intel.com>
2012-06-26 13:12:57 -0700

    modified:   project/Build.scala

--- project/Build.scala
+++ project/Build.scala
@@ -11,8 +11,27 @@ object ApplicationBuild extends Build {
       // Add your project dependencies here,
     )
 
-    val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
-      // Add your own project settings here      
-    )
 
+	    def publishArtifact(task: TaskKey[File]) = addArtifact(artifact in (Compile, task), task in Compile)
+
+	    val buildSettings: Seq[Setting[_]] = Defaults.defaultSettings ++ Seq(
+	      name := appName,
+	      version := appVersion,
+	      organization := "com.automatedlabs.demo",
+
+	      scalaVersion := "2.9.1",
+
+	      publishTo <<= version { (v: String) =>
+	        val nexus = "http://ec2-174-129-75-167.compute-1.amazonaws.com:8080/nexus/"
+	        if (v.trim.endsWith("SNAPSHOT")) 
+	          Some("snapshots" at nexus + "content/repositories/snapshots") 
+	        else
+	          Some("releases"  at nexus + "content/repositories/releases")
+	      },
+	      credentials += Credentials(Path.userHome / ".ivy2" / ".credentials")
+
+	    ) ++ 
+	    addArtifact(Artifact(appName, "bundle", "zip"), dist)
+
+	    val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA, settings = buildSettings)
 }
