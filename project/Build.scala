3b3dbd68ad063e8fad4f5431c14a352d0075000d
Mark Cooper <mark.cooper@intel.com>
2012-06-26 13:40:12 -0700

    modified:   project/Build.scala

--- project/Build.scala
+++ project/Build.scala
@@ -11,27 +11,25 @@ object ApplicationBuild extends Build {
       // Add your project dependencies here,
     )
 
+    def publishArtifact(task: TaskKey[File]) = addArtifact(artifact in (Compile, task), task in Compile)
 
-	    def publishArtifact(task: TaskKey[File]) = addArtifact(artifact in (Compile, task), task in Compile)
+    val buildSettings: Seq[Setting[_]] = Defaults.defaultSettings ++ Seq(
+	 name := appName,
+	 version := appVersion,
+	 organization := "com.intelmedia.demo",
 
-	    val buildSettings: Seq[Setting[_]] = Defaults.defaultSettings ++ Seq(
-	      name := appName,
-	      version := appVersion,
-	      organization := "com.automatedlabs.demo",
+	 scalaVersion := "2.9.1",
 
-	      scalaVersion := "2.9.1",
+	 publishTo <<= version { (v: String) =>
+	   val nexus = "http://ec2-174-129-75-167.compute-1.amazonaws.com:8080/nexus/"
+	   if (v.trim.endsWith("SNAPSHOT")) 
+	       Some("snapshots" at nexus + "content/repositories/snapshots") 
+	   else
+	       Some("releases"  at nexus + "content/repositories/releases")
+	  }, credentials += Credentials(Path.userHome / ".ivy2" / ".credentials")
 
-	      publishTo <<= version { (v: String) =>
-	        val nexus = "http://ec2-174-129-75-167.compute-1.amazonaws.com:8080/nexus/"
-	        if (v.trim.endsWith("SNAPSHOT")) 
-	          Some("snapshots" at nexus + "content/repositories/snapshots") 
-	        else
-	          Some("releases"  at nexus + "content/repositories/releases")
-	      },
-	      credentials += Credentials(Path.userHome / ".ivy2" / ".credentials")
+	 ) ++ 
+	 addArtifact(Artifact(appName, "bundle", "zip"), dist)
 
-	    ) ++ 
-	    addArtifact(Artifact(appName, "bundle", "zip"), dist)
-
-	    val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA, settings = buildSettings)
+	 val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA, settings = buildSettings)
 }
